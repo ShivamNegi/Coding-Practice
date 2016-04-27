47ee16ad5e7152c460fda7127c9f4db27179758b
ShivamNegi <shivamnegi2019@gmail.com>
2016-04-27 22:10:58 +0530

    Adding college programs

---
+++ College/DBS/factorial.sql
@@ -0,0 +1,14 @@
+declare
+x number;
+fact number;
+begin
+x := &x;
+fact := 1;
+
+for i in 1 .. x
+	loop
+	fact := fact * i;
+	end loop;
+dbms_output.put_line('' + fact);
+end;
+/
---
+++ DailyProgrammer/Easy/Verify3x3magicsquare/Python/verifymagic.py
@@ -0,0 +1,9 @@
+arr = []
+
+for i in range(9):
+	no = int(raw_input())
+	arr.append(no)
+
+if sum(arr[:3]) != 15:
+	print False
+else if su	

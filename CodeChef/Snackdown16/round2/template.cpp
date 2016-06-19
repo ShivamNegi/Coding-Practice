8e9e40df1f3c5f8ce842e35835e2da70dfcee029
ShivamNegi <shivamnegi2019@gmail.com>
2016-06-19 19:50:06 +0530

    round2

---
+++ CodeChef/Snackdown16/round2/template.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+bool isprime(int no)
+{
+    for(int i = 2; i < sqrt(no); i++)
+    {
+        if(no % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int gcd(int a, int b)
+{
+    if(a % b == 1)
+        return b;
+    else
+        return (gcd(b, a % b));
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        vector<int> a, primes;
+        int even = 0;
+
+
+        for(int i = 0; i < n; i++)
+        {
+            int no;
+            cin>>no;
+            if(no % 2 == 0 && no != 2)
+            {
+                even++;
+            }
+            else if(isprime(no))
+            {
+                // primes++;
+                // i need to check if the prime is already present
+                if(find(primes))
+                    primes.push_back(no);
+            }
+            else
+                a.push_back(no);
+        }
+
+        int max_size = 0;
+
+        for(int i = 0; i < a.size(); i++)
+        {
+
+            for(int j = 0; j < a.size(); j++)
+            {
+                
+            }
+
+            if(msize > max_size)
+                max_size = msize;
+        }
+
+        int val;
+
+        if(even > 0)
+            val = max_size + primes.size() + 1;
+        else
+            val = max_size + primes.size();
+
+        cout<<val<<endl;
+    }
+    return 0;
+}

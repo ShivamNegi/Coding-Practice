a4457612e5d7508b4d1673d0d8765c1da5c81f88
ShivamNegi <shivamnegi2019@gmail.com>
2017-05-08 08:32:21 +0530

    Cleaning up, Removing redundant folders

--- CodeChef/Snackdown16/prelim1a/NDIFFPAL.py
+++
@@ -1,7 +0,0 @@
-from string import ascii_lowercase as l
-
-for t in range(int(raw_input())):
-    no = int(raw_input())
-    out = l * (no / 26)
-    out = out + l[0: no%26]
-    print out
--- DailyProgrammer/Easy/Verify3x3magicsquare/Python/verifymagic.py
+++
@@ -1,9 +0,0 @@
-arr = []
-
-for i in range(9):
-	no = int(raw_input())
-	arr.append(no)
-
-if sum(arr[:3]) != 15:
-	print False
-else if su	
